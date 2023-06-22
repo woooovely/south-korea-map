b8453b3354f31620c07231bdbd2bf2380c2486eb
woosang <woosang3078@hanmail.net>
2023-06-22 10:55:24 +0900

    feat: 지도 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
-import Script from 'next/script';
+import useMap from '@/utils/map';
+import type { NextPage } from 'next';
+import styled from 'styled-components'
 
-const Home = () => {
+const Home: NextPage = () => {
+  useMap();
 
-  return (
-    
-  )
+  return <MapContainer id="map"></MapContainer>
 }
+
+const MapContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+`
 
 export default Home;
