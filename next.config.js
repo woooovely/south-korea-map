51e0c89d7d4f0a91171c4427aec7d43c6d6ad8bd
woosang <woosang3078@hanmail.net>
2023-06-22 10:55:41 +0900

    chore: 프로젝트 세팅

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    KAKAO_API_KEY: 'd5090c6e1d78be7e7ddd1b99d9eb3503'
-  }
 }
 
 module.exports = nextConfig
